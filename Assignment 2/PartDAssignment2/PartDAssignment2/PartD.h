4fee7feb4d3543e90d2c18acef74839adacd0aed
Taher Kathawala <47065855+TIK1999@users.noreply.github.com>
2020-12-04 15:23:42 -0800

    Update PartD.h

--- Assignment 2/PartDAssignment2/PartDAssignment2/PartD.h
+++ Assignment 2/PartDAssignment2/PartDAssignment2/PartD.h
@@ -72,8 +72,23 @@ void display_student_menu(){
 	
 	cout << "Welcome Student!\n";
 	cout << "Please select from the following options:\n";
-	cout << "Press 1 to enter program rankings\n";
-	cout << "Press 2 to enter personal statement\n";
+	cout << "Press 1 to enter your program rankings\n";
+	cout << "Press 2 to enter your personal statement\n";
+	cout << "Press ~ to log out any time\n";
+	
+	int select_menu_option;
+	
+	if(select_menu_option == 1){
+		input_rankings();
+	} else if (select_menu_option == 2){
+		input_statement();
+	} else if (select_menu_option == '~'){
+		cout << "You are being logged out...\n";
+		return;
+	} else {}
+	
+	"You are being logged out...\n";
+	
 }
 
 void display_admin_menu(){
@@ -90,8 +105,8 @@ void display_admin_menu(){
 		if(select_menu_option == 1){
 			score_statements(); // score_statements is a loop
 			
-		} else if (select_menu_option = ~) {
-			display << "You are being logged out...\n";
+		} else if (select_menu_option = '~') {
+			cout << "You are being logged out...\n";
 			return;
 		} else {}
 
